ff879f7ba93c8c9f53d91078c8c4f1950287e2b3
Carlos Ramos <carlosramosdev@protonmail.com>
2022-07-28 19:03:01 +0200

    date format bugfix

--- lib/src/utils/date_format.dart
+++ lib/src/utils/date_format.dart
@@ -1,24 +1,24 @@
 extension DateFormat on DateTime {
   String get formatDMY {
     final year = this.year;
-    final month = this.month > 10 ? "${this.month}" : "0${this.month}";
-    final day = this.day > 10 ? "${this.day}" : "0${this.day}";
+    final month = this.month >= 10 ? "${this.month}" : "0${this.month}";
+    final day = this.day >= 10 ? "${this.day}" : "0${this.day}";
 
     return "$day/$month/$year";
   }
 
   String get formatMDY {
     final year = this.year;
-    final month = this.month > 10 ? "${this.month}" : "0${this.month}";
-    final day = this.day > 10 ? "${this.day}" : "0${this.day}";
+    final month = this.month >= 10 ? "${this.month}" : "0${this.month}";
+    final day = this.day >= 10 ? "${this.day}" : "0${this.day}";
 
     return "$month/$day/$year";
   }
 
   String get formatYMD {
     final year = this.year;
-    final month = this.month > 10 ? "${this.month}" : "0${this.month}";
-    final day = this.day > 10 ? "${this.day}" : "0${this.day}";
+    final month = this.month >= 10 ? "${this.month}" : "0${this.month}";
+    final day = this.day >= 10 ? "${this.day}" : "0${this.day}";
 
     return "$year/$month/$day";
   }
--- test/date_format_test.dart
+++ test/date_format_test.dart
@@ -3,17 +3,17 @@ import 'package:monster_hunter_app/src/utils/date_format.dart';
 
 void main() {
   test('date format test', () {
-    final date = DateTime(2010, 2, 1);
-    expect(date.formatDMY, "01/02/2010");
+    final date = DateTime(2010, 2, 10);
+    expect(date.formatDMY, "10/02/2010");
   });
 
   test('date format MDY', () {
-    final date = DateTime(2010, 2, 1);
-    expect(date.formatMDY, "02/01/2010");
+    final date = DateTime(2010, 12, 1);
+    expect(date.formatMDY, "12/01/2010");
   });
 
   test('date format YMD', () {
-    final date = DateTime(2010, 2, 1);
-    expect(date.formatYMD, "2010/02/01");
+    final date = DateTime(2010, 12, 10);
+    expect(date.formatYMD, "2010/12/10");
   });
 }
