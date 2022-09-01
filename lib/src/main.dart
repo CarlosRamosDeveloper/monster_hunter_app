fd6ae1c200e4ea3bbc7609fdbf54b3559bbb4138
Carlos Ramos <carlosramosdev@protonmail.com>
2022-09-01 10:20:30 +0200

    bloc implementation in main

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/themes_bloc/themes_bloc.dart';
 import 'dependencies/di.dart' as di;
 
 import 'package:monster_hunter_app/src/screens/screens.dart';
@@ -8,20 +10,35 @@ import 'themes/app_themes.dart';
 
 void main() async {
   await di.init();
-  runApp(const MyApp());
+  BlocOverrides.runZoned(() => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(
+          create: (context) => ThemesBloc()
+            ..add(const SwitchThemeEvent(theme: AppTheme.nargacuga)))
+    ], child: const App());
+  }
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Monster Hunter ',
-      theme: AppTheme.nargacuga.theme.appThemeData,
-      home: const HomeScreen(),
-      routes: AppRoute.getAppRoutes(),
+    return BlocBuilder<ThemesBloc, ThemesState>(
+      builder: (context, state) {
+        return MaterialApp(
+          title: 'Monster Hunter ',
+          theme: AppTheme.nargacuga.theme.appThemeData,
+          home: const SettingsScreen(),
+          routes: AppRoute.getAppRoutes(),
+        );
+      },
     );
   }
 }
