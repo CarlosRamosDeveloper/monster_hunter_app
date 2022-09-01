e22ea9925a729c7429fb3a45cf188bec98f7ac7a
Carlos Ramos <carlosramosdev@protonmail.com>
2022-09-01 08:12:24 +0200

    Added Homescreen to routing

--- lib/src/route/app_route.dart
+++ lib/src/route/app_route.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:monster_hunter_app/src/screens/home/homescreen.dart';
 
 import '../models/models.dart';
 
 class AppRoute {
-  static final routeOption = <RouteOption>[];
+  static const initialRoute = HomeScreen();
+  static final routeOption = <RouteOption>[
+    RouteOption(
+        route: 'home', name: 'Página Principal', page: const HomeScreen())
+  ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+
+    appRoutes.addAll({'home': (BuildContext context) => const HomeScreen()});
 
     for (final route in routeOption) {
       appRoutes.addAll({route.route: (BuildContext context) => route.page});
