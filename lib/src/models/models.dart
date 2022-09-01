360e9c3ae9f72dec4f53cf9980fbdd8ee3655610
Carlos Ramos <carlosramosdev@protonmail.com>
2022-09-01 07:20:06 +0200

    RouteOption class added

---
+++ lib/src/models/models.dart
@@ -0,0 +1,2 @@
+export 'player_data_model.dart';
+export 'route_option.dart';
---
+++ lib/src/models/route_option.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RouteOption {
+  final String route;
+  final String name;
+  final Widget page;
+  RouteOption({
+    required this.route,
+    required this.name,
+    required this.page,
+  });
+}
